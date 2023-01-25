eecd4ac378c65ab900f0da48a9df8f3082dd4f73
Kevin Salazar <deeiqh@gmail.com>
2023-01-25 07:44:02 -0500

    chore: add sendotp and validateotp guards

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,4 +1,7 @@
+import { UseGuards } from '@nestjs/common';
 import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { SendOtpGuard } from './guards/send-otp.guard';
+import { ValidateOtpGuard } from './guards/validate-otp.guard';
 
 @Resolver()
 export class AppResolver {
@@ -8,6 +11,8 @@ export class AppResolver {
   }
 
   @Mutation(() => String)
+  @UseGuards(SendOtpGuard)
+  @UseGuards(ValidateOtpGuard)
   async getCar(): Promise<string> {
     return 'a car';
   }
